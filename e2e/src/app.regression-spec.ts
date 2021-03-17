e9ba5a3efa295e47a3683a6e41bb637ebbd79f35
Kurt Johnson <kurt.johnson@headspring.com>
2021-03-17 14:26:30 -0500

    Add Protractor test suites
    Adds test suites for accepance, regression, and smoke

---
+++ e2e/src/app.regression-spec.ts
@@ -0,0 +1,153 @@
+import { browser, element, by, ElementFinder, ElementArrayFinder } from 'protractor';
+
+const expectedH1 = 'Tour of Heroes';
+const expectedTitle = `${expectedH1}`;
+const targetHero = { id: 15, name: 'Magneta' };
+const targetHeroDashboardIndex = 3;
+const nameSuffix = 'X';
+const newHeroName = targetHero.name + nameSuffix;
+
+class Hero {
+  id: number;
+  name: string;
+
+  // Factory methods
+
+  // Hero from string formatted as '<id> <name>'.
+  static fromString(s: string): Hero {
+    return {
+      id: +s.substr(0, s.indexOf(' ')),
+      name: s.substr(s.indexOf(' ') + 1),
+    };
+  }
+
+  // Hero from hero list <li> element.
+  static async fromLi(li: ElementFinder): Promise<Hero> {
+    const stringsFromA = await li.all(by.css('a')).getText();
+    const strings = stringsFromA[0].split(' ');
+    return { id: +strings[0], name: strings[1] };
+  }
+
+  // Hero id and name from the given detail element.
+  static async fromDetail(detail: ElementFinder): Promise<Hero> {
+    // Get hero id from the first <div>
+    const id = await detail.all(by.css('div')).first().getText();
+    // Get name from the h2
+    const name = await detail.element(by.css('h2')).getText();
+    return {
+      id: +id.substr(id.indexOf(' ') + 1),
+      name: name.substr(0, name.lastIndexOf(' '))
+    };
+  }
+}
+
+describe('Regression Tests', () => {
+
+  beforeAll(() => browser.get(''));
+
+  function getPageElts() {
+    const navElts = element.all(by.css('app-root nav a'));
+
+    return {
+      navElts,
+
+      appDashboardHref: navElts.get(0),
+      appDashboard: element(by.css('app-root app-dashboard')),
+      topHeroes: element.all(by.css('app-root app-dashboard > div a')),
+
+      appHeroesHref: navElts.get(1),
+      appHeroes: element(by.css('app-root app-heroes')),
+      allHeroes: element.all(by.css('app-root app-heroes li')),
+      selectedHeroSubview: element(by.css('app-root app-heroes > div:last-child')),
+
+      heroDetail: element(by.css('app-root app-hero-detail > div')),
+
+      searchBox: element(by.css('#search-box')),
+      searchResults: element.all(by.css('.search-result li'))
+    };
+  }
+
+  describe('Dashboard tests', () => {
+
+    beforeAll(() => browser.get(''));
+
+    it('has top heroes', async () => {
+      const page = getPageElts();
+      expect(await page.topHeroes.count()).toEqual(4);
+    });
+
+    it(`selects and routes to ${targetHero.name} details`, dashboardSelectTargetHero);
+
+    it(`updates hero name (${newHeroName}) in details view`, updateHeroNameInDetailView);
+
+    it(`cancels and shows ${targetHero.name} in Dashboard`, async () => {
+      await element(by.buttonText('go back')).click();
+      await browser.waitForAngular(); // seems necessary to gets tests to pass for toh-pt6
+
+      const targetHeroElt = getPageElts().topHeroes.get(targetHeroDashboardIndex);
+      expect(await targetHeroElt.getText()).toEqual(targetHero.name);
+    });
+
+    it(`selects and routes to ${targetHero.name} details`, dashboardSelectTargetHero);
+
+    it(`updates hero name (${newHeroName}) in details view`, updateHeroNameInDetailView);
+
+    it(`saves and shows ${newHeroName} in Dashboard`, async () => {
+      await element(by.buttonText('save')).click();
+      await browser.waitForAngular(); // seems necessary to gets tests to pass for toh-pt6
+
+      const targetHeroElt = getPageElts().topHeroes.get(targetHeroDashboardIndex);
+      expect(await targetHeroElt.getText()).toEqual(newHeroName);
+    });
+
+  });
+
+  async function dashboardSelectTargetHero() {
+    const targetHeroElt = getPageElts().topHeroes.get(targetHeroDashboardIndex);
+    expect(await targetHeroElt.getText()).toEqual(targetHero.name);
+    await targetHeroElt.click();
+    await browser.waitForAngular(); // seems necessary to gets tests to pass for toh-pt6
+
+    const page = getPageElts();
+    expect(await page.heroDetail.isPresent()).toBeTruthy('shows hero detail');
+    const hero = await Hero.fromDetail(page.heroDetail);
+    expect(hero.id).toEqual(targetHero.id);
+    expect(hero.name).toEqual(targetHero.name.toUpperCase());
+  }
+
+  async function updateHeroNameInDetailView() {
+    // Assumes that the current view is the hero details view.
+    await addToHeroName(nameSuffix);
+
+    const page = getPageElts();
+    const hero = await Hero.fromDetail(page.heroDetail);
+    expect(hero.id).toEqual(targetHero.id);
+    expect(hero.name).toEqual(newHeroName.toUpperCase());
+  }
+
+});
+
+async function addToHeroName(text: string): Promise<void> {
+  const input = element(by.css('input'));
+  await input.sendKeys(text);
+}
+
+async function expectHeading(hLevel: number, expectedText: string): Promise<void> {
+  const hTag = `h${hLevel}`;
+  const hText = await element(by.css(hTag)).getText();
+  expect(hText).toEqual(expectedText, hTag);
+}
+
+function getHeroAEltById(id: number): ElementFinder {
+  const spanForId = element(by.cssContainingText('li span.badge', id.toString()));
+  return spanForId.element(by.xpath('..'));
+}
+
+function getHeroLiEltById(id: number): ElementFinder {
+  const spanForId = element(by.cssContainingText('li span.badge', id.toString()));
+  return spanForId.element(by.xpath('../..'));
+}
+
+async function toHeroArray(allHeroes: ElementArrayFinder): Promise<Hero[]> {
+  return allHeroes.map(Hero.fromLi);
+}
